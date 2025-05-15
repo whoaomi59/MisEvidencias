b84100d3ba929d249f47f974c98ef056d1b7468e
whoaomi59 <whoaomi11@gmail.com>
2025-05-15 16:59:51 -0500

    dfg

--- Front/src/view/home/index.jsx
+++ Front/src/view/home/index.jsx
@@ -34,7 +34,7 @@ export default function MesesPanel() {
   };
 
   return (
-    <div className="p-4 sm:p-6 space-y-6">
+    <div className="max-w-5xl mx-auto p-4 sm:p-6 space-y-10">
       <h1 className="text-xl sm:text-2xl font-bold">Mis Evidencias</h1>
 
       {/* Crear nuevo mes */}
